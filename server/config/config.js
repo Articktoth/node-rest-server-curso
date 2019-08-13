ff9da9a39eac14a6695a3f3629571b000678df8f
Alejandro Soto <tothdevelop@gmail.com>
2019-08-13 13:24:35 -0400

    Rest server -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,18 @@
 //  PUERTO
 //=============
 process.env.PORT = process.env.PORT || 3000;
+
+//ENTORNO
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//base de datos
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB=  'mongodb://localhost:27017/cafe'
+}else{
+    urlDB= 'mongodb+srv://toto:jo8zdipAPBMeC9WI@nodecurso-kuybu.mongodb.net/test?retryWrites=true&w=majority';
+}
+
+process.env.URLDB = urlDB;
